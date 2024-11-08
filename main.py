5944a1c88324bf45c95025d5e139480ec824dbdb
izeta5 <roli.izeta33@gmail.com>
2024-11-08 15:10:27 -0600

    Connection to DB

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("Manu")
-print("Is the goat")
+import sqlite3
+try:
+    connection = sqlite3.connect('Database for Expense tracker.db')
+    cursor = connection.cursor()
+    print('Database Initialized')
+
+    query = 'select sqlite_version();'
+    cursor.execute(query)
+
+    result = cursor.fetchall()
+    print('SQLite version is {}'.format(result))
+
+    cursor.close()
+    
+except sqlite3.Error as error:
+    print('Error occured - ', error)
+    
+finally:
+   
+    if connection:
+        connection.close()
+        print('SQLite Connection closed')
+
