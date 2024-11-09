78baa8d38fecfbafa0e532a53489affb6752304d
izeta5 <roli.izeta33@gmail.com>
2024-11-08 19:47:08 -0600

    show all users

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import sqlite3
 
+# Connect to DB
 connection = sqlite3.connect('Database for Expense tracker.db')
 cursor = connection.cursor()
 print('Database Initialized')
 
+def show_Users():
+    cursor.execute("SELECT rowid, * FROM Users")
+    people = cursor.fetchall()
+    
+    for person in people:
+        print(f"{person}")
 
+show_Users()
+# To send changes to DB
 connection.commit()
 
+# To close DB
 cursor.close()
 connection.close()
 print('SQLite Connection closed')
